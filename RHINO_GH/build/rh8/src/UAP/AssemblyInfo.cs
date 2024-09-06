94966d956289970088dc0aafc1262c24eb1b5e50
Clark <clark.cheng@uapcompany.com>
2024-09-06 16:59:17 -0400

    Build 0.0.1

---
+++ RHINO_GH/build/rh8/src/UAP/AssemblyInfo.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Reflection;
+using System.Runtime.Versioning;
+using System.Runtime.InteropServices;
+
+using Rhino.PlugIns;
+
+[assembly: PlugInDescription(DescriptionType.Organization, "UAP")]
+[assembly: PlugInDescription(DescriptionType.Email, "clark.cheng@uapcompany.com")]
+[assembly: PlugInDescription(DescriptionType.Phone, "")]
+[assembly: PlugInDescription(DescriptionType.Address, "")]
+[assembly: PlugInDescription(DescriptionType.Country, "")]
+[assembly: PlugInDescription(DescriptionType.WebSite, "")]
+[assembly: PlugInDescription(DescriptionType.UpdateUrl, "")]
+[assembly: PlugInDescription(DescriptionType.Icon, "UAP.Resources.projectIcon.ico")]
+
+[assembly: AssemblyTitle("UAP")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyCompany("UAP")]
+[assembly: AssemblyProduct("UAP")]
+[assembly: AssemblyCopyright("Copyright © 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("99a1eda8-81c6-4967-b683-d7dcd01e92ab")]
+[assembly: AssemblyVersion("0.1.1.9016")]
+[assembly: AssemblyFileVersion("0.1.1.9016")]
+[assembly: AssemblyInformationalVersion("0.1.1.9016")]
+
+[assembly: TargetFramework(".NETFramework,Version=v4.8", FrameworkDisplayName = ".NET Framework 4.8")]
