5b46b15f880f1dee6ac07be9795646fe648ad631
Clark <clark.cheng@uapcompany.com>
2024-09-09 11:39:07 -0400

    Create SelSameArea.py

---
+++ RHINO_GH/_SOURCE CODE/SelSameArea.py
@@ -0,0 +1,47 @@
+import rhinoscriptsyntax as rs
+import math as m
+
+
+#Get geometry
+area_obj=rs.GetObjects("Select Geo",preselect=True)
+if not area_obj:
+    print("None Selected")
+
+else:
+    #create list to hold geo
+    measured_obj=[]
+    
+    for obj in area_obj:
+
+        #for each obj measure the area
+        area=rs.Area(obj)
+        area=round(area,3)
+        
+        #debugging
+        #print(area)
+
+        #debugging
+        measured_obj.append(area)
+    print(measured_obj)
+    
+    unique=set(measured_obj)
+    #print(unique)
+    
+    occur={}
+    for i in measured_obj:
+        occur[i]=occur.get(i,0)+1
+    
+    mapped=[occur[i]>1 for i in measured_obj]
+
+    print(mapped)
+
+
+    #print(measured_obj)    
+    #if len(measured_obj) !=len(set(measured_obj)):
+        #print("equal")
+
+    #else:
+        #print("None")
+
+
+
