88e79ac9ce053d87c2f67fbb703488168ec26c42
Clark <clark.cheng@uapcompany.com>
2024-09-10 14:40:28 -0400

    Update NametoLayers.py

--- 01_RHINO/_SOURCE CODE/NametoLayers.py
+++ 01_RHINO/_SOURCE CODE/NametoLayers.py
@@ -1,4 +1,6 @@
 import rhinoscriptsyntax as rs
+import random
+from System.Drawing import Color
 
 
 name_to_layer=rs.GetObjects()
@@ -9,7 +11,24 @@ names=[]
 
 for obj in name_to_layer:
     a=rs.ObjectName(obj)
-    names.append(a)
+
+    if a==None:
+        #Creates red layer to store no named objs
+        rs.AddLayer("None", color=Color.Red)
+        rs.ObjectLayer(obj, "None")
+    else:
+
+
+
+        #random color for layers
+        randcolor=random.randint(0,255),random.randint(0,255),random.randint(0,255)
+        #print(randcolor)
+
+        #add layering names
+        rs.AddLayer(a,color=(randcolor))
+
+        rs.ObjectLayer(obj,a)
+        names.append(a)
 print(names)
 
 
