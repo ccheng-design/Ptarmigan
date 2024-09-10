7e844e15fb82c6fa113317c0bbb114f4099a606c
Clark <clark.cheng@uapcompany.com>
2024-09-10 16:54:53 -0400

    Update VolumetoDot.py

--- 01_RHINO/_SOURCE CODE/VolumetoDot.py
+++ 01_RHINO/_SOURCE CODE/VolumetoDot.py
@@ -6,43 +6,43 @@ from System.Drawing import Color
 #Surface Volume
 #Gets the volume of each object and adds a text dot
 
+#filter goemetry types
+geo_types=rs.filter.polysurface | rs.filter.surface
 
-volume=rs.GetObjects()
+volume=rs.GetObjects("Select PolySrf",preselect=True, filter=geo_types)
 if not volume: print("None Selected")
 
+
+#creating list of volumes and open_srfs
 volume_list=[]
 open_polysrf_list=[]
 
 for obj in volume:
     #measure volume
     a=rs.SurfaceVolume(obj)
+
+    #mark objects red for all open polysrfs
     if a==None:
         bad=rs.ObjectColor(obj,Color.Red)
         open_polysrf_list.append(bad)
         
+    
     else:
         #centroid as a guid
         centroid=rs.SurfaceVolumeCentroid(obj)
 
-
-        #print(a[0])
-        
-        
+        #add to list for counting
         volume_list.append(a[0])
 
+
+        #define string; can also use a f-string as well
         txt_dot_string="Volume: "+str(round(a[0],3))+" cubic "+str(rs.UnitSystemName(False,False,False))
-        print(txt_dot_string)
+        
 
         #string vol_info="Volume", round(a[0],3)
         rs.AddTextDot(txt_dot_string,centroid[0])
+
+
+#Output stats for what worked/didnt work
 print(len(volume_list), "objects were successfully calculated.", len(open_polysrf_list), "Are Marked in Red")
 print("Objects in Red are Not Closed Polysurfaces")
-#print(len(open_polysrf_list))
-    
-
-
-
-
-
-
-
