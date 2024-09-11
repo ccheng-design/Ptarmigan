d7d8fcf9e662da29504c0bff935b46ee45d9557a
Clark <clark.cheng@uapcompany.com>
2024-09-11 10:38:58 -0400

    Update AutoName.py

--- 01_RHINO/_SOURCE CODE/AutoName.py
+++ 01_RHINO/_SOURCE CODE/AutoName.py
@@ -1,24 +1,56 @@
 
 import rhinoscriptsyntax as rs
-
+import random
 
 #AutoName
 #Creates name for each obj
 #1. Establish naming convention
 #2. Select objs in order
 #3. Create layers with the manually entered naming convention
+#4. Add text dot at obj
 
 
 #Get string prefix
 prefix=rs.GetString("Name/Abbreviation for Part")
-print(prefix)
+#print(prefix)
 
 #Get objs
-parts=rs.GetObjects()
+parts=rs.GetObjects("Select in Order of Labels")
 if not parts: print("None Selected")
 
 else:
-    print(len(parts))
+    #print(len(parts))
 
 
+    values=[]
+    
+    start=rs.GetInteger("Start Value",1)
+    step=rs.GetInteger("Step Value",1)
+    count=len(parts)
+
+    series=range(start, start + count * step, step)
+    seq=[]
+    #randcolor=[]
+
+
+    for i in series:
+        labels=str(prefix)+str(i)
+        #print(labels)
+
+        seq.append(labels)
+        color=random.randint(0,255),random.randint(0,255),random.randint(0,255)
+        #print(color)
+        rs.AddLayer(labels,color=(color))
+
+    for obj in zip(parts,seq):
+        rs.ObjectLayer(obj, seq)
+            
+        
+    
+
+
+    #print(randcolor)
+    
+
+    
 
