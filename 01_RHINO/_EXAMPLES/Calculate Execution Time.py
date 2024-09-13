24a7b14aaaf428597388ed69ba475e7e7493a40a
Clark <clark.cheng@uapcompany.com>
2024-09-13 11:02:40 -0400

    Create Calculate Execution Time.py

---
+++ 01_RHINO/_EXAMPLES/Calculate Execution Time.py
@@ -0,0 +1,10 @@
+import time
+start_time = time.time()
+
+#Function for how long it takes
+print("test test")
+
+
+end_time = time.time()
+execution_time = start_time - end_time
+print("Execution time:",execution_time)
