9564cf5fc4a1f20c277612a8b91eb9dfa226d694
Clark <clark.cheng@uapcompany.com>
2024-09-24 11:26:07 -0400

    Update BOM.py

--- 01_RHINO/_SOURCE CODE/BOM.py
+++ 01_RHINO/_SOURCE CODE/BOM.py
@@ -13,18 +13,36 @@ def block_list_count():
         block_list=[]
         counts={}
 
+        block_desc=[]
+        desc_counts={}
+
         #for each object in the layer, test if its a block
         for obj in layer:
             if rs.IsBlockInstance(obj):
                 block_names=rs.BlockInstanceName(obj)
                 block_list.append(block_names)
 
+        #get block description
+        for names in block_list:      
+                a=rs.BlockDescription(names)
+                block_desc.append(a)
+        
+
+        
         #for each string in block_list list
         for string in block_list:
             if string in counts:
                 counts[string]+=1
             else:
                 counts[string]=1
+
+        #for each string in block_desc list
+        for string in block_desc:
+            if string in desc_counts:
+                desc_counts[string]+=1
+            else:
+                desc_counts[string]=1
+        print(desc_counts)
 
         #create lists for 
         key_list=[]
@@ -35,14 +53,32 @@ def block_list_count():
             values_list.append(counts[string])
 
         #debugging
-        print(key_list)
-        print(values_list)
+        #print(key_list)
+        #print(values_list)
+
+        key_list_length=[]
+        for string in key_list:
+            key_list_length.append(len(string))
+
+        
+        
+        max_length=max(key_list_length)
+        #debugging
+        #print(max_length)
 
         pt=rs.GetPoint("Pick Point")
-        #pt=Rhino.Geometry.Point3d(0,0,0)
 
-        spacingy=0.12
-        spacingx=1
+        #Text Height
+                #text_height=0.1
+
+        text_height=rs.GetReal("Text_Height",0.1)
+        if text_height is None:
+            return
+        
+        
+
+        spacingy=text_height+0.15
+        spacingx=max_length*text_height
         vector_y=Rhino.Geometry.Vector3d(0,-spacingy,0)
         vector_x=Rhino.Geometry.Vector3d(spacingx,0,0)
 
@@ -54,16 +90,17 @@ def block_list_count():
         scaled_vector=vector_x*length
 
 
-
-        text_height=0.1
-
-        print(pt+scaled_vector)
+        
+        #Debugging
+        #print(pt+scaled_vector)
 
         #Add text
         for items in key_list:
             pt+=vector_y
-            rs.AddPoint(pt)
-            #pt+scaled_vector
+
+            #debugging
+            #rs.AddPoint(pt)
+            
 
             #Top Line
             offset_y=Rhino.Geometry.Vector3d(0,(text_height/2)+0.01,0)
@@ -72,13 +109,19 @@ def block_list_count():
             #Bottom Line
             rs.AddLine(pt-offset_y,(pt+scaled_vector-offset_y))
 
+            #Left Line
+            rs.AddLine((pt+offset_y),(pt-offset_y))
+
+            #Right Line
+            rs.AddLine((pt+scaled_vector+offset_y),(pt+scaled_vector-offset_y))
 
             rs.AddText(items,pt,text_height,justification=131073)
 
         for i in values_list:
             number=number+vector_y
 
-            rs.AddPoint(number)
+            #debugging
+            #rs.AddPoint(number)
 
             rs.AddText(i,number,text_height,justification=131076)
 
@@ -100,9 +143,9 @@ if results==[True]:
         block_list_count()
 
 else:
-    print("it works")
-    #Choose between all blocks or BOM layer
-    names=rs.BlockNames(False)
-    if names:
-        for n in names:
-            print(n)
+    
+    #Get all blocks from Rhino Environment
+    layer=rs.ObjectsByType(4096)
+    #print(layer)
+    
+    block_list_count()
