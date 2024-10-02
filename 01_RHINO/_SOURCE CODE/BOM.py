ff6a20513a4e8dcbebabc9d92d94db7440b962eb
Clark <clark.cheng@uapcompany.com>
2024-10-01 20:12:41 -0400

    Update BOM.py

--- 01_RHINO/_SOURCE CODE/BOM.py
+++ 01_RHINO/_SOURCE CODE/BOM.py
@@ -1,5 +1,6 @@
 import rhinoscriptsyntax as rs
 import Rhino
+from collections import Counter
 
 #BOM
 #Creates bill of materials based on the blocks being in the BOM layer (user defined)
@@ -22,12 +23,15 @@ def block_list_count():
                 block_names=rs.BlockInstanceName(obj)
                 block_list.append(block_names)
 
+        #Sorts block list alphabetically
+        block_list.sort()
+
         #get block description
         for names in block_list:      
                 a=rs.BlockDescription(names)
                 block_desc.append(a)
         
-
+        
         
         #for each string in block_list list; counting the amount of unique blocks
         for string in block_list:
@@ -35,12 +39,16 @@ def block_list_count():
                 counts[string]+=1
             else:
                 counts[string]=1
-        
+
+
 
         #for each string in block_desc list
         #create list of unique parts
         unique_blk_names=set(block_list)
 
+        #print(unique_blk_names)
+
+        
         #string list of mapped indices
         mapped_index={}
         index=0
@@ -49,7 +57,12 @@ def block_list_count():
             mapped_index[string]=index
             index=index+1
 
+################################################
+        test=Counter(block_desc)
+        count_values=list(test.values())
 
+        
+        
         #store the set of mapped indices
         mapped_indices=[]
 
@@ -61,8 +74,12 @@ def block_list_count():
         for i in mapped_indices:
             desc_counts.append(block_desc[i])
 
+        #desc_counts.reverse()
+        print(count_values)
+        print(block_desc)
+        print(mapped_indices)
         print(desc_counts)
-        
+##################################################################
         #create lists for 
         key_list=[]
         values_list=[]
@@ -78,10 +95,10 @@ def block_list_count():
         for string in desc_counts:
             
             if string is None: 
-                uniq_desc.append("0")
+                uniq_desc.append("გამარჯობა")
             else: uniq_desc.append(string)
 
-        print(uniq_desc)
+        
 
         #Getting maximum length for the strings of block names
         key_list_length=[]
@@ -114,7 +131,7 @@ def block_list_count():
         spacingy=text_height+0.15
         spacingx_name=max_length_names*text_height*1.15
         spacingx_desc=(max_length_names*text_height)+(max_length_desc*text_height*0.7)
-        spacingx_amt=(max_length_names*text_height)+(max_length_desc*text_height)+0.3
+        spacingx_amt=(max_length_names*text_height)+(max_length_desc*text_height)+0.2
 
         vector_y=Rhino.Geometry.Vector3d(0,-spacingy,0)
         vector_x_names=Rhino.Geometry.Vector3d(spacingx_name,0,0)
@@ -130,30 +147,28 @@ def block_list_count():
 
         vector_x_names.Unitize() #unitize
         length=spacingx_name
-        scaled_vector=vector_x_names*length
+        scaled_vector=vector_x_names*length*0.8
 
 
         
         #Debugging
         #print(pt+scaled_vector)
 
-        middle=[]
         left=[]
+        left_middle=[]
+        right_middle=[]
+        right=[]
 
         #Add text
         for items in key_list:
             pt+=vector_y
 
-            #rs.AddPoint(pt)
+            rs.AddPoint(pt)
             
 
             #Top Line
             offset_y=Rhino.Geometry.Vector3d(0,(spacingy/2),0)
             rs.AddLine(pt+offset_y,(pt+scaled_vector+offset_y))
-            
-
-            
-            #rs.AddPoint(pt+blk_desc)
 
 
             #Bottom Line
@@ -162,11 +177,11 @@ def block_list_count():
             #Left Line
             l=rs.AddLine((pt+offset_y),(pt-offset_y))
 
-            #middle Line
-            m=rs.AddLine((pt+scaled_vector+offset_y),(pt+scaled_vector-offset_y))
+            #Left Middle Line
+            lm=rs.AddLine((pt+scaled_vector+offset_y),(pt+scaled_vector-offset_y))
 
 
-            middle.append(m)
+            left_middle.append(lm)
             left.append(l)
 
             rs.AddText(items,pt,text_height,justification=131073)
@@ -177,15 +192,22 @@ def block_list_count():
             #debugging
             rs.AddPoint(x_dir_desc)
             
-            
+            #Right Middle Line
+            rm=rs.AddLine((x_dir_desc+offset_y),(x_dir_desc-offset_y))
+            right_middle.append(rm)
+
             rs.AddText(i,x_dir_desc,text_height,justification=131076)
 
         for items in values_list: #count of each block listed
             blk_desc+=vector_y
 
             #Right Line
-            rs.AddLine((blk_desc-(vector_y/2)),(blk_desc+(vector_y/2)))
+            r=rs.AddLine((blk_desc-(vector_y/2)),(blk_desc+(vector_y/2)))
+            right.append(r)
+
+
             
+
             pt=vector_y
 
             rs.AddPoint(pt)
@@ -194,14 +216,24 @@ def block_list_count():
             rs.AddText(items,blk_desc,text_height,justification=131076)
             #
 
-        #Join middle side of curves and simplify
-        if len(middle)!=1:
-            a=rs.JoinCurves(middle,True)
+        #Join left middle side of curves and simplify
+        if len(left_middle)!=1:
+            a=rs.JoinCurves(left_middle,True)
             rs.SimplifyCurve(a)
 
         #Join left side of curves and simplify
         if len(left)!=1:
             a=rs.JoinCurves(left,True)
+            rs.SimplifyCurve(a)
+        
+        #Join right middle side of curves and simplify
+        if len(right_middle)!=1:
+            a=rs.JoinCurves(right_middle,True)
+            rs.SimplifyCurve(a)
+
+        #Join right side of curves and simplify
+        if len(right)!=1:
+            a=rs.JoinCurves(right,True)
             rs.SimplifyCurve(a)
 
 
