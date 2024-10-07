873b5cf73cb8d21ffa3661b3e3cbbd3ab12317ce
Clark <clark.cheng@uapcompany.com>
2024-10-07 12:32:13 -0400

    Create Sanity Check Template.cs

---
+++ 02_GH/_EXAMPLES/Sanity Check Template.cs
@@ -0,0 +1,36 @@
+ //Sanity check examples
+ 
+ //Sanity
+        //Checks if Data is null/void
+        if (INPUT==null)
+        {
+            this.AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Input parameter Points failed to collect data");
+            return;
+        }
+
+        
+        
+        
+
+
+        //Checks by count
+        int pt_count=Points.Count();
+
+        if (INPUT==0)
+        {
+            this.AddRuntimeMessage(GH_RuntimeMessageLevel.Warning, "Input parameter Point failed to collect data");
+            return;
+        }
+
+        //Message box below example
+        int Jarvis_count=Jarvis_pts.Count();
+        this.Component.Message=Jarvis_count.ToString() + " Point(s)";
+
+
+        //DEFAULT VALUE
+
+        //Use to refernce string if not list
+        if (string.IsNullOrEmpty(Pattern))Pattern=defaultPattern;
+
+        //Use this if in a list
+        if (Pattern ==null) Pattern=defaultPattern;
