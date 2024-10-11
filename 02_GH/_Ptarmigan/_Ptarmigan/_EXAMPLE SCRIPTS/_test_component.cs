5e107b6803ae3cc40583d8b136b6f10840188d8f
Clark <clark.cheng@uapcompany.com>
2024-10-11 14:57:13 -0400

    _ptarmigan build

--- 02_GH/_Ptarmigan/_Ptarmigan/_EXAMPLE SCRIPTS/_test_component.cs
+++ 02_GH/_Ptarmigan/_Ptarmigan/_EXAMPLE SCRIPTS/_test_component.cs
@@ -3,11 +3,11 @@ using Grasshopper.Kernel;
 using Rhino.Geometry;
 using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.Reflection;
 
-namespace MyGrasshopperAssembly1
+namespace _Ptarmigan
 {
-    public class MyGrasshopperAssemblyComponent2 : GH_Component
+    public class _test_component : GH_Component
     {
         /// <summary>
         /// Each implementation of GH_Component must provide a public 
@@ -16,10 +16,10 @@ namespace MyGrasshopperAssembly1
         /// Subcategory the panel. If you use non-existing tab or panel names, 
         /// new tabs/panels will automatically be created.
         /// </summary>
-        public MyGrasshopperAssemblyComponent2()
-          : base("Convex Hull", "CH",
-            "test convex",
-            "Category", "Subcategory")
+        public _test_component()
+          : base("test_component", "test",
+            "Creates points based on 3 points.",
+            "Ptarmigan", "test")
         {
         }
 
@@ -28,9 +28,10 @@ namespace MyGrasshopperAssembly1
         /// </summary>
         protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
         {
-            //USE FOR INPUT WIRES
-            pManager.AddPointParameter("Points", "Pts", "Points to calc", GH_ParamAccess.list);
-            pManager.AddPointParameter('Points', "Pts","Points to calc", GH_ParamAccess.item,0);
+            pManager.AddNumberParameter("X", "x", "X Coordinate", GH_ParamAccess.item, 0);
+            pManager.AddNumberParameter("Y", "y", "Y Coordinate", GH_ParamAccess.item, 1);
+            pManager.AddNumberParameter("Z", "z", "Z Coordinate", GH_ParamAccess.item, 2);
+
         }
 
         /// <summary>
@@ -38,9 +39,7 @@ namespace MyGrasshopperAssembly1
         /// </summary>
         protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
         {
-            //USE FOR OUTPUT WIRES
-            pManager.AddPointParameter("Hull Points", "Hull Pts", "All outer points", GH_ParamAccess.item);
-            pManager.AddCurveParameter("Polyline", "PLine", "Polyline Drawn", GH_ParamAccess.item);
+            pManager.AddPointParameter("Point", "pt", "Constructed Point", GH_ParamAccess.item);
         }
 
         /// <summary>
@@ -48,18 +47,22 @@ namespace MyGrasshopperAssembly1
         /// </summary>
         /// <param name="DA">The DA object can be used to retrieve data from input parameters and 
         /// to store data in output parameters.</param>
-        protected override async void SolveInstance(IGH_DataAccess DA)
+        protected override void SolveInstance(IGH_DataAccess DA)
         {
-            //USE FOR INPUT VALUE
-            List<Point3d> Points= new List<Point3d>();
-            DA.GetDataList(0, Points);
-            DA.GetDataList(0, ref Points);
+            //construct point
+            double x = 0;
+            double y = 0;
+            double z = 0;
+
+            DA.GetData(0, ref x);
+            DA.GetData(1, ref y);
+            DA.GetData(2, ref z);
+
+            Point3d point = new Point3d(x, y, z);
+
+            DA.SetData(0, point);
+
             
-
-            //USE FOR OUTPUT VALUE
-            DA.SetData(0, Hull_Points);
-            DA.SetData(1, polyline);
-
         }
 
         /// <summary>
@@ -83,6 +86,6 @@ namespace MyGrasshopperAssembly1
         /// It is vital this Guid doesn't change otherwise old ghx files 
         /// that use the old ID will partially fail during loading.
         /// </summary>
-        public override Guid ComponentGuid => new Guid("92f50079-8f41-4988-ae8a-d360a9ff6d28");
+        public override Guid ComponentGuid => new Guid("6b641dc9-aab3-4d73-a1a0-55b563e9bb0b");
     }
 }
---
+++ 02_GH/_Ptarmigan/_Ptarmigan/_PtarmiganInfo.cs
@@ -0,0 +1,29 @@
+﻿using Grasshopper;
+using Grasshopper.Kernel;
+using System;
+using System.Drawing;
+
+namespace _Ptarmigan
+{
+    public class _PtarmiganInfo : GH_AssemblyInfo
+    {
+        public override string Name => "_Ptarmigan";
+
+        //Return a 24x24 pixel bitmap to represent this GHA library.
+        public override Bitmap Icon => null;
+
+        //Return a short string describing the purpose of this GHA library.
+        public override string Description => "";
+
+        public override Guid Id => new Guid("052bd0ca-6c62-441e-8d16-546a148ebcec");
+
+        //Return a string identifying you or your company.
+        public override string AuthorName => "Clark Cheng";
+
+        //Return a string representing your preferred contact details.
+        public override string AuthorContact => "clark.cheng@uapcompany.com";
+
+        //Return a string representing the version.  This returns the same version as the assembly.
+        public override string AssemblyVersion => GetType().Assembly.GetName().Version.ToString();
+    }
+}
