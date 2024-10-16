24a4f8728fa7eb83b0e14ed6586d8e070db766a9
Clark <clark.cheng@uapcompany.com>
2024-10-16 15:49:56 -0400

    build 0.3.11-beta

---
+++ _BUILDS/rh8/src/UAP/ProjectCommand_ef6b6cd3.cs
@@ -0,0 +1,32 @@
+using System;
+
+using Rhino;
+using Rhino.Commands;
+
+namespace RhinoCodePlatform.Rhino3D.Projects.Plugin
+{
+  [CommandStyle(Rhino.Commands.Style.ScriptRunner)]
+  public class ProjectCommand_ef6b6cd3 : Command
+  {
+    public Guid CommandId { get; } = new Guid("ef6b6cd3-45de-4ac6-912c-f85de769890d");
+
+    public ProjectCommand_ef6b6cd3() { Instance = this; }
+
+    public static ProjectCommand_ef6b6cd3 Instance { get; private set; }
+
+    public override string EnglishName => "SelSameArea";
+
+    protected override Rhino.Commands.Result RunCommand(RhinoDoc doc, RunMode mode)
+    {
+      // NOTE:
+      // Initialize() attempts to loads the core rhinocode plugin
+      // and prepare the scripting platform. This call can not be in any static
+      // ctors of Command or Plugin classes since plugins can not be loaded while
+      // rhino is loading this plugin. The call has an initialized check and is
+      // very fast after the first run.
+      ProjectPlugin.Initialize();
+
+      return ProjectPlugin.RunCode(this, CommandId, doc, mode);
+    }
+  }
+}
