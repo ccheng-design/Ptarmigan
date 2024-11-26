00f31049dc59345942ddebdacec4afb21a2c38b7
Clark <clark.cheng@uapcompany.com>
2024-11-26 12:41:07 -0500

    build update

--- 02_GH/_Ptarmigan/_Ptarmigan/obj/Debug/net7.0-windows/_Ptarmigan.AssemblyInfo.cs
+++ 02_GH/_Ptarmigan/_Ptarmigan/obj/Debug/net7.0-windows/_Ptarmigan.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("Description of _Ptarmigan")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+3e85025ab5eb6e24e2b0943222df9666a7024131")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+82f55819db288128e843bb7ce75783b63fbb576a")]
 [assembly: System.Reflection.AssemblyProductAttribute("_Ptarmigan")]
 [assembly: System.Reflection.AssemblyTitleAttribute("_Ptarmigan")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
