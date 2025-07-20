6e3a11dfc540b548f72838a224986cf24039e662
Clark Cheng <clark.cheng@uapcompany.com>
2025-07-20 14:20:06 +0700

    minkowski sum

--- 02_GH/_Ptarmigan/_Ptarmigan/obj/Debug/net7.0-windows/_Ptarmigan.AssemblyInfo.cs
+++ 02_GH/_Ptarmigan/_Ptarmigan/obj/Debug/net7.0-windows/_Ptarmigan.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("Description of _Ptarmigan")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+fb27f5436767946c17b50952d9b46d78f19474df")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+886040bfbc3d66b00a9921b78acd5ebf6972148f")]
 [assembly: System.Reflection.AssemblyProductAttribute("_Ptarmigan")]
 [assembly: System.Reflection.AssemblyTitleAttribute("_Ptarmigan")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
