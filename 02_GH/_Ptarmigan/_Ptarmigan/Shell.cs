c4a15bd56f83eaddfd2df7ecd7c71d3ffe357ab7
Clark Cheng <clark.cheng@uapcompany.com>
2025-10-25 16:36:53 +1300

    Update Shell.cs

--- 02_GH/_Ptarmigan/_Ptarmigan/Shell.cs
+++ 02_GH/_Ptarmigan/_Ptarmigan/Shell.cs
@@ -179,9 +179,9 @@ namespace _Ptarmigan
         {
             get
             {
-                //You can add image files to your project resources and access them like this:
-                // return Resources.IconForThisComponent;
-                return null;
+                var assembly = System.Reflection.Assembly.GetExecutingAssembly();
+                var stream = assembly.GetManifestResourceStream("_Ptarmigan.Resources.Shell-24px.png");
+                return new System.Drawing.Bitmap(stream);
             }
         }
 
