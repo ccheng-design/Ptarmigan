badd1b96c30ac4a78a19e96de851ae3f8fb1916f
Clark Cheng <clark.cheng@uapcompany.com>
2025-12-21 16:51:37 -0500

    Update DetailScale.py

--- 01_RHINO/_SOURCE CODE/DetailScale.py
+++ 01_RHINO/_SOURCE CODE/DetailScale.py
@@ -1,27 +1,3 @@
-"""
-NOTE:
-
-- Reference to RhinoCommmon.dll is added by default
-
-- You can specify your script requirements like:
-
-    # r: <package-specifier> [, <package-specifier>]
-    # requirements: <package-specifier> [, <package-specifier>]
-
-    For example this line will ask the runtime to install
-    the listed packages before running the script:
-
-    # requirements: pytoml, keras
-
-    You can install specific versions of a package
-    using pip-like package specifiers:
-
-    # r: pytoml==0.10.2, keras>=2.6.0
-
-- Use env directive to add an environment path to sys.path automatically
-    # env: /path/to/your/site-packages/
-"""
-#! python3
 
 import rhinoscriptsyntax as rs
 import scriptcontext as sc
@@ -83,15 +59,15 @@ if mode == "DS":
 #scale factor
 elif mode == "SF":
     
-    modelSfactor=1/rs.GetReal("Scale Factor",1,0)
+    modelSfactor=rs.GetReal("Scale Factor",1,0)
     if modelSfactor == 0:
         print("No Value Given")
         raise SystemExit()
 
- #viewport scale
+#viewport scale
 elif mode == "VS":
    
-    modelSfactor=rs.GetReal("Viewport Scale Factor",1,0)
+    modelSfactor=1/rs.GetReal("Viewport Scale Factor",1,0)
     if modelSfactor ==0:
         print("No Value Given")
         raise SystemExit()
