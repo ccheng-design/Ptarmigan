906e1d98031ee00bc191413de8c7cd38ecda3785
Clark Cheng <clark.cheng@uapcompany.com>
2026-01-20 19:01:04 +1300

    Create DetailScaleFactor.py

---
+++ 01_RHINO/_SOURCE CODE/DetailScaleFactor.py
@@ -0,0 +1,54 @@
+
+
+import rhinoscriptsyntax as rs
+import scriptcontext as sc
+import math
+
+import System
+import System.Collections.Generic
+import Rhino
+
+import math
+import sys
+
+
+from fractions import Fraction
+from decimal import Decimal
+
+rs.MessageBox("Select details and text in the same order!", buttons=0)
+
+
+detailGet=rs.GetObjects("Get Details",filter=32768)
+description=rs.GetObjects("Get Text",filter=4096)
+
+drawingScaleFactor=[]
+
+print(detailGet)
+for i in detailGet:
+    detailObj=sc.doc.Objects.FindId(i)
+
+    #print(detailObj)
+
+    viewport=detailObj.DetailGeometry.PageToModelRatio
+
+
+    #print(viewport)
+
+    x=Fraction(viewport).limit_denominator()
+    fracScale=Fraction(12/x.denominator)
+
+    drawingScale=str(fracScale) + '"' + " = " + "1'-0"+'"'
+    #print(drawingScale)
+    drawingScaleFactor.append(drawingScale)
+
+    #print(str(fracScale) + '"' + " = " + "1'-0"+'"')
+
+#print(description)
+for i,j in zip(description,drawingScaleFactor):
+  
+    print(j)
+    rs.SetUserText(i, "SCALE", j)
+
+
+
+
