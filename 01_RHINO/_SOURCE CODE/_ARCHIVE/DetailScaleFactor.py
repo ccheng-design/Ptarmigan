458bc9ddcf23e73b021d6ecf9271a2f08cc84065
Clark Cheng <clark.cheng@uapcompany.com>
2026-02-15 12:56:28 +1300

    Create DetailScaleFactor.py

---
+++ 01_RHINO/_SOURCE CODE/_ARCHIVE/DetailScaleFactor.py
@@ -0,0 +1,80 @@
+
+
+import rhinoscriptsyntax as rs
+import scriptcontext as sc
+import math
+
+import System
+import System.Collections.Generic
+import Rhino
+
+import math
+import sys
+
+
+from fractions import Fraction
+from decimal import Decimal
+
+
+#Uses embedded text with attributes to define the scale factor
+rs.MessageBox("Select details and text in the same order!", buttons=0, title="DetailScaleFactor")
+
+
+detailGet=rs.GetObjects("Get Details",filter=32768)
+description=rs.GetObjects("Get Text",filter=4096)
+
+#Exit clause
+if description or detailGet is None:
+    print("Nothing Selected")
+
+
+
+drawingScaleFactor=[]
+descriptionCount=[]
+
+#description count
+for i in description:
+    descriptionCount.append(i)
+
+#detail count
+detailGetCount=[]
+for i in detailGet:
+    detailGetCount.append(i)
+
+
+print(len(descriptionCount))
+print(len(detailGetCount))
+
+if len(descriptionCount) != len(detailGetCount):
+    sys.exit()
+
+print(detailGet)
+for i in detailGet:
+    detailObj=sc.doc.Objects.FindId(i)
+
+    #print(detailObj)
+
+    viewport=detailObj.DetailGeometry.PageToModelRatio
+
+
+    #print(viewport)
+
+    x=Fraction(viewport).limit_denominator()
+    fracScale=Fraction(12/x.denominator)
+
+    drawingScale=str(fracScale) + '"' + " = " + "1'-0"+'"'
+    #print(drawingScale)
+    drawingScaleFactor.append(drawingScale)
+
+    #print(str(fracScale) + '"' + " = " + "1'-0"+'"')
+
+#print(description)
+for i,j in zip(description,drawingScaleFactor):
+  
+    print(j)
+    rs.SetUserText(i, "SCALE", j)
+    
+
+
+
+
