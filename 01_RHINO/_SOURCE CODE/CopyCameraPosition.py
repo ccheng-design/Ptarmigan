5b55efda0fd923c8df275137c3d9522126081341
Clark Cheng <clark.cheng@uapcompany.com>
2026-03-06 16:32:56 +1300

    Update CopyCameraPosition.py

--- 01_RHINO/_SOURCE CODE/CopyCameraPosition.py
+++ 01_RHINO/_SOURCE CODE/CopyCameraPosition.py
@@ -9,32 +9,71 @@ import System
 import System.Collections.Generic
 import Rhino
 
+import sys
 
-detailRef=rs.GetObjects("Get Details",filter=32768,preselect=True)
 
-#detailChange=rs.GetObjects("Detail to Change", filter=32768, preselect=False)
-for i in detailRef:
+detailRef=rs.GetObject("Get Reference Detail to Copy From",filter=32768,preselect=True)
+if detailRef is None:
+    sys.exit()
 
+detailChange=rs.GetObjects("Detail to Change", filter=32768, preselect=False)
+
+
+
+detail = sc.doc.Objects.FindId(detailRef)
+
+#CONVERT TO RHINOVIEWPORT CLASS
+viewport = detail.Viewport
+
+X = viewport.CameraX
+Y = viewport.CameraY
+Z = viewport.CameraZ
+location = viewport.CameraLocation
+direction = viewport.CameraDirection
+target = viewport.CameraTarget
+scale = detail.DetailGeometry.PageToModelRatio
+parallel = viewport.IsParallelProjection
+
+
+cam_plane = Rhino.Geometry.Plane(location, Z)
+
+true_X = Rhino.Geometry.Vector3d(1,0,0)
+
+rot_angle = Rhino.Geometry.Vector3d.VectorAngle(X,true_X,cam_plane)
+
+print(360-(rot_angle*(180/math.pi)))
+
+print("VectorX:", X)
+print(Y)
+print(Z)
+print(direction)
+print(parallel)
+print("Target:", target)
+
+
+
+
+
+for i in detailChange:
     detail = sc.doc.Objects.FindId(i)
 
-    #CONVERT TO RHINOVIEWPORT CLASS
     viewport = detail.Viewport
 
-    X = viewport.CameraX
-    Y = viewport.CameraY
-    Z = viewport.CameraZ
-    location = viewport.CameraLocation
-    direction = viewport.CameraDirection
+    if parallel is True:
+        viewport.ChangeToParallelProjection(True)
+    
 
-    cam_plane = Rhino.Geometry.Plane(location, Z)
+    #viewport.SetCameraTarget(target, True)
+    viewport.SetCameraLocations(target, location)
+    
 
-    true_X = Rhino.Geometry.Vector3d(1,0,0)
+    
 
-    rot_angle = Rhino.Geometry.Vector3d.VectorAngle(X,true_X,cam_plane)
+    detail.CommitChanges()
+    detail.CommitViewportChanges()
+    detail.CommitChanges()
 
-    print(360-(rot_angle*(180/math.pi)))
+sc.doc.Views.Redraw()
 
-    print(X)
-    print(Y)
-    print(Z)
-    print(direction)
+#SCALE FACTOR CHANGE
+rs.DetailScale(i,1,scale)
