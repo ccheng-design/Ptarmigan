17a29204744eda5e37eaed558a8d8a3fb1821509
Clark Cheng <cchengg@umich.edu>
2026-04-10 00:36:11 -0400

    Update CameraPositionFromDetail.py

--- 01_RHINO/_SOURCE CODE/CameraPositionFromDetail.py
+++ 01_RHINO/_SOURCE CODE/CameraPositionFromDetail.py
@@ -12,72 +12,82 @@ import Rhino
 import sys
 
 
-detailRef=rs.GetObject("Get Reference Detail to Copy From",filter=32768,preselect=True)
-if detailRef is None:
-    sys.exit()
-
-detailChange=rs.GetObjects("Detail to Change", filter=32768, preselect=False)
+def CameraPositionFromDetail():
 
 
+    detailRef=rs.GetObject("Get Reference Detail to Copy From",filter=32768,preselect=True)
+    if detailRef is None:
+        return
 
-detail = sc.doc.Objects.FindId(detailRef)
-
-#CONVERT TO RHINOVIEWPORT CLASS
-viewport = detail.Viewport
-
-X = viewport.CameraX
-Y = viewport.CameraY
-Z = viewport.CameraZ
-location = viewport.CameraLocation
-direction = viewport.CameraDirection
-target = viewport.CameraTarget
-scale = detail.DetailGeometry.PageToModelRatio
-parallel = viewport.IsParallelProjection
-lenslength = viewport.Camera35mmLensLength
+    detailChange=rs.GetObjects("Detail to Change", filter=32768, preselect=False)
+    if detailChange is None:
+        return
 
 
-cam_plane = Rhino.Geometry.Plane(location, Z)
+    #Detail to a Guid
+    detail = sc.doc.Objects.FindId(detailRef)
 
-true_X = Rhino.Geometry.Vector3d(1,0,0)
-
-rot_angle = Rhino.Geometry.Vector3d.VectorAngle(X,true_X,cam_plane)
-
-print(360-(rot_angle*(180/math.pi)))
-
-print("VectorX:", X)
-print(Y)
-print(Z)
-print(direction)
-print(parallel)
-print("Target:", target)
-
-
-
-
-
-
-for i in detailChange:
-    detail = sc.doc.Objects.FindId(i)
-
+    #CONVERT TO RHINOVIEWPORT CLASS
     viewport = detail.Viewport
 
-    if parallel is True:
-        viewport.ChangeToParallelProjection(True)
-    else:
-        viewport.ChangeToPerspectiveProjection(True,lenslength)
-    
+    #Getting Viewport Properties
+    X = viewport.CameraX
+    Y = viewport.CameraY
+    Z = viewport.CameraZ
+    location = viewport.CameraLocation
+    direction = viewport.CameraDirection
+    target = viewport.CameraTarget
+    scale = detail.DetailGeometry.PageToModelRatio
+    parallel = viewport.IsParallelProjection
+    lenslength = viewport.Camera35mmLensLength
 
-    #viewport.SetCameraTarget(target, True)
-    viewport.SetCameraLocations(target, location)
-    
 
-    
+    cam_plane = Rhino.Geometry.Plane(location, Z)
+    true_X = Rhino.Geometry.Vector3d(1,0,0)
+    rot_angle = Rhino.Geometry.Vector3d.VectorAngle(X,true_X,cam_plane)
 
-    detail.CommitChanges()
-    detail.CommitViewportChanges()
-    detail.CommitChanges()
+    """
+    print(360-(rot_angle*(180/math.pi)))
 
-sc.doc.Views.Redraw()
+    print("VectorX:", X)
+    print(Y)
+    print(Z)
+    print(direction)
+    print(parallel)
+    print("Target:", target)
 
-#SCALE FACTOR CHANGE
-rs.DetailScale(i,1,scale)
+    """
+
+
+
+
+
+    for i in detailChange:
+        detail = sc.doc.Objects.FindId(i)
+
+        viewport = detail.Viewport
+
+        if parallel is True:
+            viewport.ChangeToParallelProjection(True)
+        else:
+            viewport.ChangeToPerspectiveProjection(True,lenslength)
+        
+
+        #viewport.SetCameraTarget(target, True)
+        viewport.SetCameraLocations(target, location)
+        
+
+        
+
+        detail.CommitChanges()
+        detail.CommitViewportChanges()
+        detail.CommitChanges()
+
+    sc.doc.Views.Redraw()
+    print("Detail View Successfully Synced.")
+
+    #SCALE FACTOR CHANGE
+    rs.DetailScale(i,1,scale)
+
+if __name__=="__main__":
+    CameraPositionFromDetail()
