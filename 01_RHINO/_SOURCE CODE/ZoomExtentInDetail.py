5df905eb4af4cc676035bbe8cb3dd331b5b43360
Clark Cheng <cchengg@umich.edu>
2026-04-10 00:36:17 -0400

    Update ZoomExtentInDetail.py

--- 01_RHINO/_SOURCE CODE/ZoomExtentInDetail.py
+++ 01_RHINO/_SOURCE CODE/ZoomExtentInDetail.py
@@ -8,14 +8,6 @@ import System
 import System.Collections.Generic
 import Rhino
 
-
-
-detailGet=rs.GetObjects("Get Details",filter=32768,preselect=True)
-
-items = ("ScaleFactor", "No", "Yes")
-zoomToFactor = rs.GetBoolean("ZoomSelectToArchScaleFactor",items,(True))
-print(zoomToFactor)
-
 #options to use
 OPTIONS = {
     "RoundUp":  "Up",
@@ -42,73 +34,92 @@ def choose_option():
         if r == Rhino.Input.GetResult.Cancel:
             return None
 
-mode=None
-if zoomToFactor[0] == True:
-    mode = choose_option()
 
-details=[]
-SF = [4,8,12,16,24,32,48,64,96,128,192,384]
-DS = []
+def ZoomExtentInDetail():
 
+    detailGet=rs.GetObjects("Get Details",filter=32768,preselect=True)
+    if detailGet is None:
+        return
 
-#RECIPROCAL OF THE SCALE FACTOR TO DRAWING SCALE FACTOR (FRACTIONAL)
-for x in SF:
-    number = 1/x
-    DS.append(number)
-print(DS)
+    items = ("ScaleFactor", "No", "Yes")
+    zoomToFactor = rs.GetBoolean("ZoomSelectToArchScaleFactor",items,(True))
+    #print(zoomToFactor)
 
-#DETAIL ADDING TO LIST
-for i in detailGet:
-    rs.DetailLock(i,False)
-    details.append(i)
-
-#ZOOM SELECTION
-currentView =rs.CurrentView()
-for i in details:
-
-    test=rs.CurrentDetail(layout=currentView,detail=i,return_name=False)
-
-    detailObj = sc.doc.Objects.FindId(i)
+    if zoomToFactor is None:
+        return
 
 
 
-    rs.AllObjects(True)
-    rs.ZoomSelected(test,True)
-    rs.UnselectAllObjects()
+    mode=None
+    if zoomToFactor[0] == True:
+        mode = choose_option()
 
-    rs.CurrentDetail(layout=currentView,detail=i)
-    
-    #print(test)
-    #print(scaleFactor)
+    details=[]
+    SF = [4,8,12,16,24,32,48,64,96,128,192,384]
+    DS = []
 
-#ROUNDING DOWN
-if zoomToFactor[0] == True and mode == "Dwn":
-    for k in details:
-        ZSdetailScale = sc.doc.Objects.FindId(k)
 
-        scaleFactor = ZSdetailScale.DetailGeometry.PageToModelRatio
-        print("ROUND DOWN", scaleFactor)
-        roundedSF = min(x for x in DS if x >= scaleFactor)
-        print(roundedSF)
+    #RECIPROCAL OF THE SCALE FACTOR TO DRAWING SCALE FACTOR (FRACTIONAL)
+    for x in SF:
+        number = 1/x
+        DS.append(number)
+    print(DS)
 
-        rs.DetailScale(k,1,roundedSF)
+    #DETAIL ADDING TO LIST
+    for i in detailGet:
+        rs.DetailLock(i,False)
+        details.append(i)
+
+    #ZOOM SELECTION
+    currentView =rs.CurrentView()
+    for i in details:
+
+        test=rs.CurrentDetail(layout=currentView,detail=i,return_name=False)
+
+        detailObj = sc.doc.Objects.FindId(i)
+
+
+
+        rs.AllObjects(True)
+        rs.ZoomSelected(test,True)
+        rs.UnselectAllObjects()
+
+        rs.CurrentDetail(layout=currentView,detail=i)
         
+        #print(test)
+        #print(scaleFactor)
 
-#ROUNDING  UP
-if zoomToFactor[0] is True and mode == "Up":
-    for k in details:
-        ZSdetailScale = sc.doc.Objects.FindId(k)
+    #ROUNDING DOWN
+    if zoomToFactor[0] == True and mode == "Dwn":
+        for k in details:
+            ZSdetailScale = sc.doc.Objects.FindId(k)
 
-        scaleFactor = ZSdetailScale.DetailGeometry.PageToModelRatio
-        print("ROUND UP", scaleFactor)
-        roundedSF = max(x for x in DS if x <= scaleFactor)
-        print(roundedSF)
+            scaleFactor = ZSdetailScale.DetailGeometry.PageToModelRatio
+            print("ROUND DOWN", scaleFactor)
+            roundedSF = min(x for x in DS if x >= scaleFactor)
+            print(roundedSF)
 
-        rs.DetailScale(k,1,roundedSF)
+            rs.DetailScale(k,1,roundedSF)
+            
+
+    #ROUNDING  UP
+    if zoomToFactor[0] is True and mode == "Up":
+        for k in details:
+            ZSdetailScale = sc.doc.Objects.FindId(k)
+
+            scaleFactor = ZSdetailScale.DetailGeometry.PageToModelRatio
+            print("ROUND UP", scaleFactor)
+            roundedSF = max(x for x in DS if x <= scaleFactor)
+            print(roundedSF)
+
+            rs.DetailScale(k,1,roundedSF)
 
 
-#DEACTIVATES DETAIL
-activeview = sc.doc.Views.ActiveView
+    #DEACTIVATES DETAIL
+    activeview = sc.doc.Views.ActiveView
 
-activeview.SetPageAsActive()
-sc.doc.Views.Redraw()
+    activeview.SetPageAsActive()
+    sc.doc.Views.Redraw()
+
+if __name__=="__main__":
+    ZoomExtentInDetail()
